7d57a98aeb3a6fec6f1d0826db5ee6bb69b7f9a7
henrique-brandao <henriquefr321@gmail.com>
2026-05-13 00:00:53 -0300

    feat: Continuing with the front-end, it initiated the first requests such as get, post, and delete.

--- backend/src/main/java/com/henrique/taskflow/controller/TaskController.java
+++ backend/src/main/java/com/henrique/taskflow/controller/TaskController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/task")
 public class TaskController {
--- frontend/taskflow/src/pages/Home/index.jsx
+++ frontend/taskflow/src/pages/Home/index.jsx
@@ -1,14 +1,34 @@
-import { useEffect } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './style.css'
 import Trash from '../../assets/trash.svg'
 import api from '../../services/api.js'
 
 function Home() {
+  const [tasks, setTasks] = useState([])
 
-  let tasks = []
+  const inputTitleRef = useRef()
+  const inputDescriptionRef = useRef()
+  
   
   async function getTask() {
-    tasks = await api.get('/task')
+    const response = await api.get('/task')
+    setTasks(response.data)
+  }
+
+  async function createTask(){
+    await api.post('/task', {
+      title: inputTitleRef.current.value,
+      description: inputDescriptionRef.current.value
+    })
+
+    inputTitleRef.current.value = ''
+    inputDescriptionRef.current.value = ''
+    getTask()
+  }
+
+  async function deleteTask(id) {
+    await api.delete(`/task/${id}`)
+    getTask()
   }
 
   useEffect(() => {
@@ -19,21 +39,21 @@ function Home() {
     <div className="container">
       <form action="" className='taskForm'>
         <h1>TaskFLow</h1>
-        <input type="text" placeholder='Title' name="Title" />
-        <input type="text" placeholder='Description' name="description" />
-        <button type='button'>Create Task</button>
+        <input type="text" placeholder='Title' name="Title" ref={inputTitleRef}/>
+        <input type="text" placeholder='Description' name="description" ref={inputDescriptionRef} />
+        <button type='button' onClick={createTask}>Create Task</button>
       </form>
 
-      {Tasks.map(task => (
+      {tasks.map(task => (
         <div key={task.id} className='taskCard'>
           <div>
             <p>Name: <span>{task.title}</span></p>
             <p>Description: <span>{task.description}</span></p>
-            <p>situation: <span>{task.situation ? 'Done' : 'Pending'}</span></p>
+            <p>situation: <span>{task.completed ? 'Done' : 'Pending'}</span></p>
             <p>Created at: <span>{task.createdAt}</span></p>
           </div>
           <button>
-            <img src={Trash} alt="Trash image" width="24" height="24" />
+            <img src={Trash} alt="Trash image" width="24" height="24" onClick={() => deleteTask(task.id)} />
           </button>
         </div>
       ))}
